4799d7804b206ae5663f58475db74f17db0c8145
Brian Lohr <brian.d.lohr@gmail.com>
2017-11-15 21:43:23 -0800

    Fixing some horrible formatting

--- CheckSales.php
+++ CheckSales.php
@@ -1,6 +1,6 @@
 <?php
 
-//TODO: Add info for $json_url
+// #TODO: Add info for $json_url
 $json_url = '';
 
 $json_data = file_get_contents($json_url);
@@ -10,15 +10,14 @@ $data = json_decode($json_data, true);
 $conversions = $data['response']['data']['data']['0']['Stat']['conversions'];
 $payout = $data['response']['data']['data']['0']['Stat']['payout'];
 
-//TODO: Add info for $link
+// #TODO: Add info for $link
 $link = mysqli_connect("localhost", "", "") or die("Error " . mysqli_error($link));
 $query = "SELECT payout, conversions FROM previous_values WHERE id = 1";
 $result = $link->query($query);
 
 $row = mysqli_fetch_array($result);
 
-if(($payout > $row["payout"]) && ($conversions > $row["conversions"]))
-{
+if(($payout > $row["payout"]) && ($conversions > $row["conversions"])) {
 	$sql = 'UPDATE previous_values SET payout = ' . $payout . ' WHERE id = 1';
 	$stmt = $link->prepare($sql);
 	$stmt->execute(); 
@@ -41,15 +40,17 @@ if(($payout > $row["payout"]) && ($conversions > $row["conversions"]))
 	}
 	$message = $new_conversions . ' new ' . $sales_text . '! $' . number_format($new_payout, 2, '.', ',') . ' made!';
 	
-	//TODO: Add Pushover info to config file
+	// #TODO: Add Pushover info to config file
 	curl_setopt_array($ch = curl_init(), array(
-	  CURLOPT_URL => "https://api.pushover.net/1/messages.json",
-	  CURLOPT_POSTFIELDS => array(
-	  "token" => "",
-	  "user" => "",
-	  "sound" => "cashregister",
-	  "message" => $message,
-	)));
+			CURLOPT_URL => "https://api.pushover.net/1/messages.json",
+			CURLOPT_POSTFIELDS => array(
+				"token" => "",
+				"user" => "",
+				"sound" => "cashregister",
+				"message" => $message,
+			)
+		)
+	);
 	curl_exec($ch);
 	curl_close($ch);
 }
